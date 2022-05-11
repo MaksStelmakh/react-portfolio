c1b71dba44dee0ce5a4e939d2b3e3e493f98fb1e
MaksStelmakh <step4ick2017@gmail.com>
2022-05-11 18:18:18 +0300

    Create HomePage and AboutPage

---
+++ src/components/Home/index.js
@@ -0,0 +1,79 @@
+import './index.scss'
+import LogoTitle from '../../assets/images/logo-s.png'
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import AnimatedLetters from '../AnimatedLetters/index'
+import Logo from './Logo'
+import Loader from 'react-loaders'
+
+const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const secondNameArray = ['t', 'e', 'l', 'm', 'a', 'k', 'h']
+  const nameArray = ['M', 'a', 'k', 's']
+  const jobArra = [
+    'w',
+    'e',
+    'b',
+    ' ',
+    'd',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+    '.',
+  ]
+
+  useEffect(() => {
+    return () => {
+      setTimeout(() => {
+        setLetterClass('text-animate-hover')
+      }, 4000)
+    }
+  }, [])
+
+  return (
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
+            <br />
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+            &nbsp;
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            />
+            <img src={LogoTitle} alt="developer" />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={secondNameArray}
+              idx={19}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArra}
+              idx={27}
+            />
+          </h1>
+          <h2>Frontend Developer / React Developer / JavaScript developer</h2>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
+        </div>
+        <Logo />
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
+}
+
+export default Home
